7fe042199fc263a5c41ddb61605b08ae57e78009
pineda.juan92 <pineda.juan92@gmail.com>
2021-12-15 19:05:25 -0500

    State created to get the string typed by the user

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.count = this.count.bind(this);
+    this.state = {
+      text: "",
+    };
+  }
+
+  count(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.count}></textarea>
+        <div className="counter">{this.state.text.length}</div>
       </div>
     );
   }
